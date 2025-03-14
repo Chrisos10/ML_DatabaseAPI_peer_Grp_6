1fdf8e1ed3bcfdfe3c0f8fa62c6bfd30b3f63086
Chrisos10 <j.dufitumuk@alustudent.com>
2025-03-14 22:55:44 +0200

    fixing hosting errors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from database import get_connection
+from .database import get_connection
 import logging
 
 # Configure logging
